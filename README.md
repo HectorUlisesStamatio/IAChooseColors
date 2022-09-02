fa9b76c73c968de9690436b000061bbe10e0cdea
HectorUlisesStamatio <ulisesstamatio@gmail.com>
2022-09-02 17:26:07 -0500

    [ADD] Js,Css & Html files

---
+++ README.md
@@ -0,0 +1,2 @@
+# IAChooseColors
+ In this project, an artificial intelligence technology is tested that selects the text color according to the darkness of the color chosen by the user for the interface along with compatibility with the dark or light theme.
---
+++ js/brainConfig.js
@@ -0,0 +1,93 @@
+    let root = document.documentElement;
+    let temaOscuro=false;
+    let cssString = '';
+    let resultado=0;
+    let fondoOscuro="#24263b"
+    let fondoBlanco="#FFFF"
+
+    let network = new brain.NeuralNetwork();
+        network.train([{input:{rojo:0,verde:0,azul:0}, output:{color:1}},
+        {input:{rojo:1,verde:1,azul:1}, output:{color:0}},
+        {input:{rojo:0,verde:1,azul:0}, output:{color:0}},
+        {input:{rojo:0,verde:.43,azul:1}, output:{color:1}},    
+        {input:{rojo:1,verde:0,azul:0}, output:{color:1}},
+        ]);
+
+
+
+    function update(color) {
+        if (temaOscuro==true){
+            tema()
+        }
+        let rgb = [color.channels.r,color.channels.g,color.channels.b];
+        let entrada = {
+            rojo: rgb[0]/255,
+            verde: rgb[1]/255,
+            azul: rgb[2]/255,
+        };
+        resultado = network.run(entrada);
+        if (resultado.color > .5) {
+            root.style.setProperty('--textcolor', "white");
+         }else{
+            root.style.setProperty('--textcolor', "black");
+         }
+
+        hexACssHsl(color.toHEXString());
+    }
+    
+
+
+      function tema() {
+        if(temaOscuro==false){
+            let cssActual =root.style.getPropertyValue('--color');
+            let cssArray = cssActual.split(',');
+            let cssNuevo = cssArray[0] +","+ cssArray[1] +","+ "30%)";
+            root.style.setProperty('--color', cssNuevo);
+            root.style.setProperty('--textcolor', "white");
+            root.style.setProperty('--fondo', fondoOscuro);
+            temaOscuro=true
+        }else{
+            root.style.setProperty('--color', cssString);
+            if (resultado.color > .5) {
+                root.style.setProperty('--textcolor', "white");
+             }else{
+                root.style.setProperty('--textcolor', "black");
+             }
+             root.style.setProperty('--fondo', fondoBlanco);
+            temaOscuro=false
+        }   
+    }
+
+
+    function hexACssHsl(hex, valuesOnly = false) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        var r = parseInt(result[1], 16);
+        var g = parseInt(result[2], 16);
+        var b = parseInt(result[3], 16);
+        
+        r /= 255, g /= 255, b /= 255;
+        var max = Math.max(r, g, b), min = Math.min(r, g, b);
+        var h, s, l = (max + min) / 2;
+        if (max == min) {
+          h = s = 0; 
+        } else {
+          var d = max - min;
+          s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+          switch(max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+          }
+          h /= 6;
+        }
+        
+        h = Math.round(h * 360);
+        s = Math.round(s * 100);
+        l = Math.round(l * 100);
+        
+        cssString = h + ',' + s + '%,' + l + '%';
+        cssString = !valuesOnly ? 'hsl(' + cssString + ')' : cssString;
+        root.style.setProperty('--color', cssString);
+        return cssString;
+      }
+    
