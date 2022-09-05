664505a985e506e4384375abd33194b47002d7c9
HectorUlisesStamatio <ulisesstamatio@gmail.com>
2022-09-04 20:39:04 -0500

    [UPDATE]  Comments styles.css & brainConfig.js

--- js/brainConfig.js
+++ js/brainConfig.js
@@ -1,3 +1,7 @@
+    /* Principal global variables
+        root = Change css global variables
+        temaOscuro = to validate if the dark theme is activated
+    */
     let root = document.documentElement;
     let temaOscuro=false;
     let cssString = '';
@@ -5,6 +9,9 @@
     let fondoOscuro="#24263b"
     let fondoBlanco="#FFFF"
 
+    /*
+      Configure the brain with the answers, these answers will serve to select the color of the text
+    */
     let network = new brain.NeuralNetwork();
         network.train([{input:{rojo:0,verde:0,azul:0}, output:{color:1}},
         {input:{rojo:1,verde:1,azul:1}, output:{color:0}},
@@ -16,9 +23,11 @@
 
 
     function update(color) {
+      /* If the dark theme is activated it will be deactivated to be able to see the color you are selecting */
         if (temaOscuro==true){
             tema()
         }
+        /* An object is created and sent to the brain */
         let rgb = [color.channels.r,color.channels.g,color.channels.b];
         let entrada = {
             rojo: rgb[0]/255,
@@ -26,6 +35,7 @@
             azul: rgb[2]/255,
         };
         resultado = network.run(entrada);
+        /* Depending on the answer, the color of the text will be selected */
         if (resultado.color > .5) {
             root.style.setProperty('--textcolor', "white");
          }else{
@@ -36,17 +46,20 @@
     }
     
 
-
+      /* It is required to pass the selected color from hex to hsl to be able to switch to dark theme */
       function tema() {
         if(temaOscuro==false){
             let cssActual =root.style.getPropertyValue('--color');
             let cssArray = cssActual.split(',');
+            /* The brightness of the color is lowered */
             let cssNuevo = cssArray[0] +","+ cssArray[1] +","+ "30%)";
+            /* Colors are changed */
             root.style.setProperty('--color', cssNuevo);
             root.style.setProperty('--textcolor', "white");
             root.style.setProperty('--fondo', fondoOscuro);
             temaOscuro=true
         }else{
+            /* If the text is white it changes to black and if it is black it changes to white */
             root.style.setProperty('--color', cssString);
             if (resultado.color > .5) {
                 root.style.setProperty('--textcolor', "white");
@@ -58,7 +71,7 @@
         }   
     }
 
-
+    /* Process to do the calculations to pass from hex to hsl */    
     function hexACssHsl(hex, valuesOnly = false) {
         var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
         var r = parseInt(result[1], 16);
